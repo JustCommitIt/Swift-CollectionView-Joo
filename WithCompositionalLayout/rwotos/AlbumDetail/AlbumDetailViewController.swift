aec962c703ddb0afba1d09a926a28bf78a930878
ohdair <ohdiar@gmail.com>
2023-05-04 16:52:28 +0900

    add: layout 균일되지 않게 수정

--- WithCompositionalLayout/rwotos/AlbumDetail/AlbumDetailViewController.swift
+++ WithCompositionalLayout/rwotos/AlbumDetail/AlbumDetailViewController.swift
@@ -80,18 +80,79 @@ extension AlbumDetailViewController {
   }
 
   func generateLayout() -> UICollectionViewLayout {
-    let itemSize = NSCollectionLayoutSize(
-      widthDimension: .fractionalWidth(1.0),
-      heightDimension: .fractionalHeight(1.0))
-    let fullPhotoItem = NSCollectionLayoutItem(layoutSize: itemSize)
+    // First Type : Full
+    let fullPhotoItem = NSCollectionLayoutItem(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1.0),
+        heightDimension: .fractionalWidth(2/3)))
+
     fullPhotoItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
 
-    let groupSize = NSCollectionLayoutSize(
-      widthDimension: .fractionalWidth(1.0),
-      heightDimension: .fractionalWidth(1/3))
-    let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: fullPhotoItem, count: 2)
+    // Second Type: Main with pair
+    // 3
+    let mainItem = NSCollectionLayoutItem(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(2/3),
+        heightDimension: .fractionalHeight(1.0)))
 
-    let section = NSCollectionLayoutSection(group: group)
+    mainItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+    // 2
+    let pairItem = NSCollectionLayoutItem(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1.0),
+        heightDimension: .fractionalHeight(0.5)))
+
+    pairItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+    let trailingGroup = NSCollectionLayoutGroup.vertical(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1/3),
+        heightDimension: .fractionalHeight(1.0)),
+      subitem: pairItem,
+      count: 2)
+
+    // 1
+    let mainWithPairGroup = NSCollectionLayoutGroup.horizontal(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1.0),
+        heightDimension: .fractionalWidth(4/9)),
+      subitems: [mainItem, trailingGroup])
+
+    // Third Type. Triplet
+    let tripletItem = NSCollectionLayoutItem(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1/3),
+        heightDimension: .fractionalHeight(1.0)))
+
+    tripletItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+
+    let tripletGroup = NSCollectionLayoutGroup.horizontal(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1.0),
+        heightDimension: .fractionalWidth(2/9)),
+      subitems: [tripletItem, tripletItem, tripletItem])
+
+    // Fourth type. Reversed main with pair
+    let mainWithPairReversedGroup = NSCollectionLayoutGroup.horizontal(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1.0),
+        heightDimension: .fractionalWidth(4/9)),
+      subitems: [trailingGroup, mainItem])
+
+    let nestedGroup = NSCollectionLayoutGroup.vertical(
+      layoutSize: NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1.0),
+        heightDimension: .fractionalWidth(16/9)),
+      subitems: [
+        fullPhotoItem,
+        mainWithPairGroup,
+        tripletGroup,
+        mainWithPairReversedGroup
+      ]
+    )
+
+    let section = NSCollectionLayoutSection(group: nestedGroup)
 
     let layout = UICollectionViewCompositionalLayout(section: section)
     return layout
